0ee4de7831ba8d3371f3012496f1051741a203c1
nathan <nathan@LAPTOP-769VABVE>
2019-07-30 20:25:01 -0400

    first committ

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyV2.Startup))]
+namespace VidlyV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyV2.Models;
+
+namespace VidlyV2.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public List<Customer> Customers { get; set; }
+    }
+}
